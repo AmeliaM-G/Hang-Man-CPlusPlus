999ea3a4185d9c999f6476b7182cce17b0a567e8
AmeliaM-G <47215882+AmeliaM-G@users.noreply.github.com>
2020-01-10 20:25:27 +0000

    initialised objects
    number of tries,
    max tries,
    possible words

---
+++ HangMan/Main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int maxTries = 3;
+int wrongGuesses = 0; //count of wrong guesses cannot exceed 3
+
+string words[] = {
+	"pyramid",
+	"document",
+	"conscience",
+	"perceive",
+	"feminine",
+	"extension",
+	"grimace",
+	"garbage",
+	"mosquito",
+	"intervention",
+	"disorder" };
+// random words with more than 5 letters from a random word generator.
+// would like to allow the code to pick random words from the dictionary itself in the future.
+
+
+
+int main() {
+	return 0;
+}
