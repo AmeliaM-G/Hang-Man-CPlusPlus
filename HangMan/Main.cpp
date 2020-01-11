b558a27ba12e8826976a6cd8575f5de528db2d89
AmeliaM-G <47215882+AmeliaM-G@users.noreply.github.com>
2020-01-11 15:42:16 +0000

    start up user display

--- HangMan/Main.cpp
+++ HangMan/Main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-const int maxTries = 3;
+const int lives = 3;
 int wrongGuesses = 0; //count of wrong guesses cannot exceed 3
 
 string words[] = {
@@ -40,7 +40,18 @@ int main() {
 	int wordLength = word.size();
 	//cout << "word is: " << word << ". Length of word is: " << wordLength << endl;
 	string blank(wordLength, '-');
-	cout << "word is: " << word << ". blank is: " << blank << endl;
+	//cout << "word is: " << word << ". blank is: " << blank << endl;
+
+	//DISPLAY TO USER:
+	cout << "Welcome to HangMan!" << endl;
+	cout << "You have to guess a randomly gnerated word of 5 characters or more!" << endl;
+	cout << "You have " << lives << " lives." << endl;
+	string start;
+	while (start != "start" && start != "START" && start != "Start") {
+		cout << "Enter \"START\" to begin." << endl;
+		cin >> start;
+	}
+	cout << "This is your word: " << blank << endl;
 
 	return 0;
 }
